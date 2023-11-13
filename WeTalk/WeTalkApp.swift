67f8e527c99e95e99187090782073c692c63fb72
pendulum445 <54065244+pendulum445@users.noreply.github.com>
2023-11-13 20:28:00 +0800

    Initial Commit

---
+++ WeTalk/WeTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeTalkApp.swift
+//  WeTalk
+//
+//  Created by liaoyunjie on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct WeTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
